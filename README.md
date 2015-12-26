39db162e8e653ef4821c33956233dc460cbf5755
Thomas Sundberg <tsu@kth.se>
2015-12-26 16:56:00 +0200

    Initiated a web app

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ Build and start the project:
 ./gradlew clean shadowJar --daemon; java -jar build/libs/julkalender-all.jar
 ```
 
+Access it at 
+[localhost:8080](http://localhost:8080)
+
 ## Decision log
 
 Motivate decisions here. We might wonder why we chose to do stuff, let this section contain our motivations.
--- src/main/java/se/kd/Main.java
+++ src/main/java/se/kd/Main.java
@@ -1,8 +1,35 @@
 package se.kd;
 
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+
 public class Main {
     public static void main(String... args) {
         HelloWorld helloWorld = new HelloWorld();
         System.out.println(helloWorld.sayHello());
+
+        int port = getPort();
+        port(port);
+        staticFileLocation("/public");
+        main();
+        awaitInitialization();
+    }
+
+    // todo rename to something better
+    private static void main() {
+        get("/", (request, response) -> {
+            Map<String, Object> map = new HashMap<>();
+            return new ModelAndView(map, "main.mustache");
+        }, new MustacheTemplateEngine());
+    }
+
+    private static int getPort() {
+        String port = System.getProperty("port", "8080");
+        return Integer.parseInt(port);
     }
 }
