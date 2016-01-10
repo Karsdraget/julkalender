4613579eb094bb62bf652c06fc4b2f551704929f
Helmfrid <mimersb@gmail.com>
2016-01-10 18:51:12 +0100

    Implemented dropdown links to old calenders

--- src/main/java/se/kd/HelloWorld.java
+++ src/main/java/se/kd/HelloWorld.java
@@ -1,7 +1,12 @@
 package se.kd;
 
+import javax.swing.*;
+
 public class HelloWorld {
     public String sayHello() {
+        return "Hello, World!";
+    }
+    public String sayHello1(Action action) {
         return "Hello, World!";
     }
 }
