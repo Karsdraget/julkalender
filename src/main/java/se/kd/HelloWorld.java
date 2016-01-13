71fa9b2162ffaf07cb8d04c5ddfce0e02661665b
Helmfrid <mimersb@gmail.com>
2016-01-13 21:23:16 +0100

    Removed javax.swing.* import from HelloWorld example

--- src/main/java/se/kd/HelloWorld.java
+++ src/main/java/se/kd/HelloWorld.java
@@ -1,12 +1,10 @@
 package se.kd;
 
-import javax.swing.*;
+
 
 public class HelloWorld {
     public String sayHello() {
         return "Hello, World!";
     }
-    public String sayHello1(Action action) {
-        return "Hello, World!";
-    }
+
 }
