02fab438d40514433143ec12badffc3ecaf2854c
Helmfrid <mimersb@gmail.com>
2016-03-02 19:34:18 +0100

    Removed javax.swing.* import from HelloWorld example

--- src/main/java/se/kd/Main.java
+++ src/main/java/se/kd/Main.java
@@ -14,9 +14,6 @@ import static spark.Spark.*;
 
 public class Main {
     public static void main(String... args) {
-        HelloWorld helloWorld = new HelloWorld();
-        System.out.println(helloWorld.sayHello());
-
         int port = getPort();
         port(port);
 
